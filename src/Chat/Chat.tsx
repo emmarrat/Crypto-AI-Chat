ba21f6a43e8f853e2cbabcca7a8eefc476af6d9a
Emir <emmarrat@gmail.com>
2023-07-11 12:45:12 +0600

    feat: added prettier

--- src/Chat/Chat.tsx
+++ src/Chat/Chat.tsx
@@ -1,99 +1,97 @@
-import React, {useEffect, useRef, useState} from 'react';
-import Sidebar from "../Components/Sidebar/Sidebar";
-import {IconButton, InputBase, Paper} from "@mui/material";
-import SendIcon from '@mui/icons-material/Send';
-import './Chat.css';
-import {MOCK_DATA_FIFTEEN} from "../constants";
+import React, { useEffect, useRef, useState } from 'react'
+import Sidebar from '../Components/Sidebar/Sidebar'
+import { IconButton, InputBase, Paper } from '@mui/material'
+import SendIcon from '@mui/icons-material/Send'
+import './Chat.css'
+import { MOCK_DATA_FIFTEEN } from '../constants'
 
 interface Message {
-  id: string,
-  text: string,
-  role: string
+	id: string
+	text: string
+	role: string
 }
 
 const Chat = () => {
-  const [messageText, setMessageText] = useState('');
-  const [messages, setMessages] = useState<Message[]>(MOCK_DATA_FIFTEEN);
-  const containerRef = useRef<HTMLDivElement>(null);
+	const [messageText, setMessageText] = useState('')
+	const [messages, setMessages] = useState<Message[]>(MOCK_DATA_FIFTEEN)
+	const containerRef = useRef<HTMLDivElement>(null)
 
-  useEffect(() => {
-    scrollToBottom();
-  }, [messages])
+	useEffect(() => {
+		scrollToBottom()
+	}, [messages])
 
-  const generateId = () => {
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#%-&*';
-    let id = '';
-    for (let i = 0; i < 12; i++) {
-      id += characters.charAt(Math.floor(Math.random() * characters.length));
-    }
-    return id;
-  };
+	const generateId = () => {
+		const characters =
+			'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#%-&*'
+		let id = ''
+		for (let i = 0; i < 20; i++) {
+			id += characters.charAt(Math.floor(Math.random() * characters.length))
+		}
+		return id
+	}
 
-  const sendMessage = (e: React.FormEvent) => {
-    e.preventDefault();
-    const userMessage = { role: 'user', text: messageText, id: generateId() };
-    console.log('your sending message = ', userMessage);
-    setMessages((prevMessages) => [...prevMessages, userMessage]);
-    setMessageText('');
-  };
+	const sendMessage = (e: React.FormEvent) => {
+		e.preventDefault()
+		const userMessage = { role: 'user', text: messageText, id: generateId() }
+		console.log('your sending message = ', userMessage)
+		setMessages(prevMessages => [...prevMessages, userMessage])
+		setMessageText('')
+	}
 
-  const scrollToBottom = () => {
-    if (containerRef.current) {
-      containerRef.current.scrollTop = containerRef.current.scrollHeight;
-    }
-  };
+	const scrollToBottom = () => {
+		if (containerRef.current) {
+			containerRef.current.scrollTop = containerRef.current.scrollHeight
+		}
+	}
 
-  const renderMessages = () => {
-    if (messages.length === 0) {
-      return <h3>Please send a message to start the dialog...</h3>;
-    }
-    return messages.map((message, index) => {
-      return (
-        <div key={index} className={`chat__msg chat__msg-${message.role}`}>
-          {message.text}
-        </div>
-      );
-    });
-  };
+	const renderMessages = () => {
+		if (messages.length === 0) {
+			return <h3>Please send a message to start the dialog...</h3>
+		}
+		return messages.map((message, index) => {
+			return (
+				<div key={index} className={`chat__msg chat__msg-${message.role}`}>
+					{message.text}
+				</div>
+			)
+		})
+	}
 
-  return (
-    <>
-      <Sidebar>
-        <div className="chat__container">
-          <div className="chat__block chat__msg-wrapp" ref={containerRef}>
-            {renderMessages()}
-          </div>
-          <div className="chat__block chat__form-wrapp">
-            <Paper component="form" onSubmit={sendMessage} className="chat__form">
-              <InputBase
-                className="chat__input"
-                type="text"
-                value={messageText}
-                onChange={(e) => setMessageText(e.target.value) }
-                placeholder="Ask me about crypto"
-                inputProps={{ 'aria-label': 'Type your message' }}
-                required
-                autoFocus
-              />
-              <div style={{ padding: '5px' }}>
-                <IconButton
-                  type="submit"
-                  disabled={messageText.length === 0}
-                  sx={{ p: '10px', color: 'secondary.dark' }}
-                  aria-label="send message"
-                >
-                  <SendIcon />
-                </IconButton>
-              </div>
-            </Paper>
-          </div>
-        </div>
-      </Sidebar>
-    </>
-  );
-};
+	return (
+		<>
+			<Sidebar>
+				<div className='chat__container'>
+					<div className='chat__block chat__msg-wrapp' ref={containerRef}>
+						{renderMessages()}
+					</div>
+					<div className='chat__block chat__form-wrapp'>
+						<Paper component='form' onSubmit={sendMessage} className='chat__form'>
+							<InputBase
+								className='chat__input'
+								type='text'
+								value={messageText}
+								onChange={e => setMessageText(e.target.value)}
+								placeholder='Ask me about crypto'
+								inputProps={{ 'aria-label': 'Type your message' }}
+								required
+								autoFocus
+							/>
+							<div style={{ padding: '5px' }}>
+								<IconButton
+									type='submit'
+									disabled={messageText.length === 0}
+									sx={{ p: '10px', color: 'secondary.dark' }}
+									aria-label='send message'
+								>
+									<SendIcon />
+								</IconButton>
+							</div>
+						</Paper>
+					</div>
+				</div>
+			</Sidebar>
+		</>
+	)
+}
 
-export default Chat;
-
-
-
+export default Chat
--- src/Components/Sidebar/Sidebar.tsx
+++ src/Components/Sidebar/Sidebar.tsx
@@ -1,175 +1,181 @@
-import React, {ReactNode} from 'react';
+import React, { ReactNode } from 'react'
 import './Sidebar.css'
 import {
-  Box,
-  CssBaseline,
-  CSSObject,
-  IconButton,
-  List,
-  ListItem,
-  ListItemButton,
-  styled,
-  Toolbar,
-  Tooltip
-} from "@mui/material";
-import MuiAppBar, {AppBarProps as MuiAppBarProps} from '@mui/material/AppBar';
-import MuiDrawer from '@mui/material/Drawer';
-import MenuIcon from '@mui/icons-material/Menu';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import ChatBubbleOutlineRoundedIcon from '@mui/icons-material/ChatBubbleOutlineRounded';
-import {Theme} from "@mui/material/styles";
-import HistoryRoundedIcon from '@mui/icons-material/HistoryRounded';
-import {HISTORY_ITEMS} from "../../constants";
+	Box,
+	CssBaseline,
+	CSSObject,
+	IconButton,
+	List,
+	ListItem,
+	ListItemButton,
+	styled,
+	Toolbar,
+	Tooltip,
+} from '@mui/material'
+import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar'
+import MuiDrawer from '@mui/material/Drawer'
+import MenuIcon from '@mui/icons-material/Menu'
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft'
+import ChatBubbleOutlineRoundedIcon from '@mui/icons-material/ChatBubbleOutlineRounded'
+import { Theme } from '@mui/material/styles'
+import HistoryRoundedIcon from '@mui/icons-material/HistoryRounded'
+import { HISTORY_ITEMS } from '../../constants'
 
-const drawerWidth = 260;
+const drawerWidth = 260
 
 const openedMixin = (theme: Theme): CSSObject => ({
-  width: drawerWidth,
-  backgroundColor: '#132D46',
-  color: '#fff',
-  transition: theme.transitions.create('width', {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.enteringScreen,
-  }),
-  overflowX: 'hidden',
-});
+	width: drawerWidth,
+	backgroundColor: '#132D46',
+	color: '#fff',
+	transition: theme.transitions.create('width', {
+		easing: theme.transitions.easing.sharp,
+		duration: theme.transitions.duration.enteringScreen,
+	}),
+	overflowX: 'hidden',
+})
 
 const closedMixin = (theme: Theme): CSSObject => ({
-  backgroundColor: '#132D46',
-  color: '#fff',
-  transition: theme.transitions.create('width', {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  overflowX: 'hidden',
-  width: 0,
-  [theme.breakpoints.up('sm')]: {
-    width: 0,
-  },
-});
+	backgroundColor: '#132D46',
+	color: '#fff',
+	transition: theme.transitions.create('width', {
+		easing: theme.transitions.easing.sharp,
+		duration: theme.transitions.duration.leavingScreen,
+	}),
+	overflowX: 'hidden',
+	width: 0,
+	[theme.breakpoints.up('sm')]: {
+		width: 0,
+	},
+})
 
-const DrawerHeader = styled('div')(({theme}) => ({
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'flex-end',
-  padding: theme.spacing(0, 1),
-  ...theme.mixins.toolbar,
-}));
+const DrawerHeader = styled('div')(({ theme }) => ({
+	display: 'flex',
+	alignItems: 'center',
+	justifyContent: 'flex-end',
+	padding: theme.spacing(0, 1),
+	...theme.mixins.toolbar,
+}))
 
 interface AppBarProps extends MuiAppBarProps {
-  open?: boolean;
+	open?: boolean
 }
 
 const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== 'open',
-})<AppBarProps>(({theme, open}) => ({
-  zIndex: theme.zIndex.drawer + 1,
-  transition: theme.transitions.create(['width', 'margin'], {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  ...(open && {
-    marginLeft: drawerWidth,
-    width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(['width', 'margin'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  }),
-}));
+	shouldForwardProp: prop => prop !== 'open',
+})<AppBarProps>(({ theme, open }) => ({
+	zIndex: theme.zIndex.drawer + 1,
+	transition: theme.transitions.create(['width', 'margin'], {
+		easing: theme.transitions.easing.sharp,
+		duration: theme.transitions.duration.leavingScreen,
+	}),
+	...(open && {
+		marginLeft: drawerWidth,
+		width: `calc(100% - ${drawerWidth}px)`,
+		transition: theme.transitions.create(['width', 'margin'], {
+			easing: theme.transitions.easing.sharp,
+			duration: theme.transitions.duration.enteringScreen,
+		}),
+	}),
+}))
 
-const Drawer = styled(MuiDrawer, {shouldForwardProp: (prop) => prop !== 'open'})(
-  ({theme, open}) => ({
-    width: drawerWidth,
-    flexShrink: 0,
-    whiteSpace: 'nowrap',
-    boxSizing: 'border-box',
-    ...(open && {
-      ...openedMixin(theme),
-      '& .MuiDrawer-paper': openedMixin(theme),
-    }),
-    ...(!open && {
-      ...closedMixin(theme),
-      '& .MuiDrawer-paper': closedMixin(theme),
-    }),
-  }),
-);
+const Drawer = styled(MuiDrawer, { shouldForwardProp: prop => prop !== 'open' })(
+	({ theme, open }) => ({
+		width: drawerWidth,
+		flexShrink: 0,
+		whiteSpace: 'nowrap',
+		boxSizing: 'border-box',
+		...(open && {
+			...openedMixin(theme),
+			'& .MuiDrawer-paper': openedMixin(theme),
+		}),
+		...(!open && {
+			...closedMixin(theme),
+			'& .MuiDrawer-paper': closedMixin(theme),
+		}),
+	}),
+)
 
 interface Props {
-  children: ReactNode;
+	children: ReactNode
 }
-const Sidebar: React.FC<Props> = ({children}) => {
-  const [open, setOpen] = React.useState(true);
+const Sidebar: React.FC<Props> = ({ children }) => {
+	const [open, setOpen] = React.useState(true)
 
-  const handleDrawerOpen = () => {
-    setOpen(true);
-  };
+	const handleDrawerOpen = () => {
+		setOpen(true)
+	}
 
-  const handleDrawerClose = () => {
-    setOpen(false);
-  };
+	const handleDrawerClose = () => {
+		setOpen(false)
+	}
 
-  return (
-    <Box sx={{display: 'flex'}}>
-      <CssBaseline/>
-      <AppBar position="fixed" open={open} sx={{
-        backgroundColor: "transparent",
-        boxShadow: 'none'
-      }}>
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            onClick={handleDrawerOpen}
-            edge="start"
-            sx={{
-              backgroundColor: '#fff',
-              marginRight: 5,
-              ...(open && {display: 'none'}),
-            }}
-          >
-            <MenuIcon sx={{
-              color: '#132D46'
-            }}/>
-          </IconButton>
-        </Toolbar>
-      </AppBar>
-      <Drawer variant="permanent" open={open}>
-        <DrawerHeader sx={{justifyContent: 'space-between', alignItems: 'center'}}>
-          <h4 className="history-title">
-            <HistoryRoundedIcon sx={{mr: 1}}/>
-            History
-          </h4>
-          <IconButton onClick={handleDrawerClose}>
-            <ChevronLeftIcon sx={{color: '#ffff'}}/>
-          </IconButton>
-        </DrawerHeader>
-        <List sx={{px: '10px'}}>
-          {HISTORY_ITEMS.map((text) => (
-            <ListItem key={text} disablePadding sx={{display: 'block'}}>
-              <Tooltip title={text} placement="right">
-                <ListItemButton
-                  sx={{
-                    minHeight: 50,
-                    justifyContent: open ? 'initial' : 'center',
-                  }}
-                >
-                  <ChatBubbleOutlineRoundedIcon sx={{mr: 1}} fontSize="small"/>
-                  <p className="history-item" style={{opacity: open ? 1 : 0}}>
-                    {text}
-                  </p>
-                </ListItemButton>
-              </Tooltip>
-            </ListItem>
-          ))}
-        </List>
-      </Drawer>
-      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-        {/*<DrawerHeader/>*/}
-        {children}
-      </Box>
-    </Box>
-  );
-};
+	return (
+		<Box sx={{ display: 'flex' }}>
+			<CssBaseline />
+			<AppBar
+				position='fixed'
+				open={open}
+				sx={{
+					backgroundColor: 'transparent',
+					boxShadow: 'none',
+				}}
+			>
+				<Toolbar>
+					<IconButton
+						color='inherit'
+						aria-label='open drawer'
+						onClick={handleDrawerOpen}
+						edge='start'
+						sx={{
+							backgroundColor: '#fff',
+							marginRight: 5,
+							...(open && { display: 'none' }),
+						}}
+					>
+						<MenuIcon
+							sx={{
+								color: '#132D46',
+							}}
+						/>
+					</IconButton>
+				</Toolbar>
+			</AppBar>
+			<Drawer variant='permanent' open={open}>
+				<DrawerHeader sx={{ justifyContent: 'space-between', alignItems: 'center' }}>
+					<h4 className='history-title'>
+						<HistoryRoundedIcon sx={{ mr: 1 }} />
+						History
+					</h4>
+					<IconButton onClick={handleDrawerClose}>
+						<ChevronLeftIcon sx={{ color: '#ffff' }} />
+					</IconButton>
+				</DrawerHeader>
+				<List sx={{ px: '10px' }}>
+					{HISTORY_ITEMS.map(text => (
+						<ListItem key={text} disablePadding sx={{ display: 'block' }}>
+							<Tooltip title={text} placement='right'>
+								<ListItemButton
+									sx={{
+										minHeight: 50,
+										justifyContent: open ? 'initial' : 'center',
+									}}
+								>
+									<ChatBubbleOutlineRoundedIcon sx={{ mr: 1 }} fontSize='small' />
+									<p className='history-item' style={{ opacity: open ? 1 : 0 }}>
+										{text}
+									</p>
+								</ListItemButton>
+							</Tooltip>
+						</ListItem>
+					))}
+				</List>
+			</Drawer>
+			<Box component='main' sx={{ flexGrow: 1, p: 3 }}>
+				{/*<DrawerHeader/>*/}
+				{children}
+			</Box>
+		</Box>
+	)
+}
 
-export default Sidebar;
+export default Sidebar
