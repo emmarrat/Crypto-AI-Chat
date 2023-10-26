5f133d7cc428546d1d0f7f1eba4a288cc4ceb88e
Emir <emmarrat@gmail.com>
2023-10-26 12:33:49 +0600

    fix: minor fixes

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,7 @@ import theme from './theme';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+const root = ReactDOM.createRoot(document.getElementById('ReactApp') as HTMLElement);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
